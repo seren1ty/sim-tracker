12cfc3afcc5c81336218f4f2fd43a79633a5382b
David Morgan <ball41@gmail.com>
2023-05-15 21:19:33 +1000

    Remove unused custom JWT fields

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,3 @@
-import Driver from '@/models/driver.model';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
@@ -9,34 +8,4 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  callbacks: {
-    async jwt({ token }) {
-      console.dir(token, { depth: null });
-
-      await Driver.findOne({ email: token.email })
-        .then((driver) => {
-          if (driver) {
-            token = {
-              ...token,
-              _id: driver._id,
-              name: driver.name,
-              isAdmin: driver.isAdmin,
-            };
-          } else {
-            console.error('Error [Driver Not Setup]: ' + token.email);
-            /* Do not support New Users at the moment
-                const newDriver = new Driver({ name, email });
-
-                newDriver.save()
-                    .then(driver => res.json(driver.name))
-                    .catch(err => res.status(400).json('Error [Add New Driver]: ' + err)); */
-          }
-        })
-        .catch((err) => {
-          console.error('Error [Get Driver]: ' + err);
-        });
-
-      return token;
-    },
-  },
 });
