5a438aecbdb7f03e09f174b0fb8b17e6d86f3580
David Morgan <ball41@gmail.com>
2023-05-18 17:30:44 +1000

    Add Authorization check post Authentication

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,5 @@
+import Driver from '@/models/driver.model';
+import dbConnect from '@/utils/db-connect';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
@@ -8,4 +10,37 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    // Login check to see if user has previously signed up / is authorized
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        await dbConnect();
+        const driverExists = await Driver.exists({ email: user.email });
+
+        if (driverExists) {
+          return true;
+        } else {
+          console.error('User not authorized [New]: ' + user.email);
+          return false;
+          /* Do not support New Users at the moment
+            const name = given_name;
+
+            const newDriver = new Driver({ name, email });
+
+            newDriver.save()
+                .then(driver => res.json(driver.name))
+                .catch(err => res.status(400).json('Error [Add New Driver]: ' + err));
+          */
+        }
+      } catch (err) {
+        console.error(
+          'User not authorized [Error]: ' + user.email + ', Error: ' + err
+        );
+        // Display a default error message
+        return false;
+        // TODO return a URL to redirect to:
+        // return '/unauthorized'
+      }
+    },
+  },
 });
